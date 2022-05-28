13c49a308a7423fa4b5720a26d8f665cc0180eb2
sajib-rana <sajibrana444@gmail.com>
2022-05-28 23:37:30 +0600

    'add'

--- index.js
+++ index.js
@@ -38,6 +38,16 @@ async function run(){
          res.send(users);
        })
 
+      app.put('/user/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const updateDoc = {
+        $set:{role:'admin'},
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send( result);
+    })
+
        app.put('/user/:email', async (req, res) => {
       const email = req.params.email;
       const user = req.body;

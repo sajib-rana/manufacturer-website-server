dce2929a37a1b9b1b8db62e260cf29257b23e5ff
sajib-rana <sajibrana444@gmail.com>
2022-05-30 16:56:01 +0600

    update

--- index.js
+++ index.js
@@ -53,14 +53,29 @@ async function run(){
          res.send(users);
        })
 
-      app.put('/user/admin/:email', async (req, res) => {
+        app.get("/admin/:email", async (req, res) => {
+          const email = req.params.email;
+          const user = await userCollection.findOne({ email: email });
+          const isAdmin = user.role === "admin";
+          res.send({ admin: isAdmin });
+        });
+
+      app.put('/user/admin/:email',verifyJWT, async (req, res) => {
       const email = req.params.email;
-      const filter = { email: email };
-      const updateDoc = {
-        $set:{role:'admin'},
-      };
-      const result = await userCollection.updateOne(filter, updateDoc);
-      res.send( result);
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({ email: requester });
+      if (requesterAccount.role === 'admin') {
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: 'admin' },
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      }
+      else{
+        res.status(403).send({message: 'forbidden'});
+      }
+
     })
 
        app.put('/user/:email', async (req, res) => {
@@ -78,13 +93,18 @@ async function run(){
      res.send({ result, token });
     })
 
-       app.get('/orders',verifyJWT, async(req, res)=>{
+       app.get('/order',verifyJWT, async(req, res)=>{
          const customer = req.query.customer;
-         const authorization = req.headers.authorization;
-         console.log('header is', authorization)
-         const query = {customer:customer}
-         const order = await orderCollection.find(query).toArray();
-         res.send(order);
+         const decodedEmail = req.decoded.email;
+         if(customer === decodedEmail){
+        const query = { customer: customer };
+        const order = await orderCollection.find(query).toArray();
+        res.send(order);
+         }
+         else{
+           return res.status(403).send({message:'forbidden access'});
+         }
+         
        })
 
         //  app.get("/myorders", JWTVerify, async (req, res) => {
